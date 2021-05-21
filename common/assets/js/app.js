1fccf16bd1e51db36aaf58c16bdcdd88a9c509dd
Benjy <benjamin.joly@live.fr>
2021-05-21 11:58:17 +0200

    fix basket js

--- common/assets/js/app.js
+++ common/assets/js/app.js
@@ -1,60 +1,42 @@
-(function ($) {
-    "use strict"; // Start of use strict
-    
-    //Basket functions
-
-    // $("#edit_form").validate();
-
-    function add_to_basket(rid) {
-        $.ajax({
-            url: 'add_to_basket.php',
-            data: "rid=" + rid + "&act=add",
-            type: 'post',
-            success: function (result) {
-                $('#b_' + rid + '_add').hide();
-                $('#b_' + rid + '_rem').show();
-            }
-        });
-    }
-
-    function rem_from_basket(rid) {
-        $.ajax({
-            url: 'add_to_basket.php',
-            data: "rid=" + rid + "&act=rem",
-            type: 'post',
-            success: function (result) {
-                $('#b_' + rid + '_rem').hide();
-                $('#b_' + rid + '_add').show();
-            }
-        });
-    }
-
-    // function clear_elt(element_id) {
-    //     $('#' + element_id).val('');
-    // }
-    // $(function () {
-    //     $(".dp").datepicker({
-    //         dateFormat: "dd/mm/yy",
-    //     });
-    // });
-
-    // Scroll to top button appear
-    $(document).on('scroll', function () {
-        var scrollDistance = $(this).scrollTop();
-        if (scrollDistance > 100) {
-            $('.scroll-to-top').fadeIn();
-        } else {
-            $('.scroll-to-top').fadeOut();
+function add_to_basket(rid) {
+    $.ajax({
+        url: 'add_to_basket.php',
+        data: "rid=" + rid + "&act=add",
+        type: 'post',
+        success: function (result) {
+            $('#b_' + rid + '_add').hide();
+            $('#b_' + rid + '_rem').show();
         }
     });
+}
 
-    // Smooth scrolling using jQuery easing
-    $(document).on('click', 'a.scroll-to-top', function (e) {
-        $('html, body').stop().animate({scrollTop:0}, 500, 'swing');
-        e.preventDefault();
+function rem_from_basket(rid) {
+    $.ajax({
+        url: 'add_to_basket.php',
+        data: "rid=" + rid + "&act=rem",
+        type: 'post',
+        success: function (result) {
+            $('#b_' + rid + '_rem').hide();
+            $('#b_' + rid + '_add').show();
+        }
     });
-})(jQuery); // End of use strict
+}
 
+// Scroll to top button appear
+$(document).on('scroll', function () {
+    var scrollDistance = $(this).scrollTop();
+    if (scrollDistance > 100) {
+        $('.scroll-to-top').fadeIn();
+    } else {
+        $('.scroll-to-top').fadeOut();
+    }
+});
+
+// Smooth scrolling using jQuery easing
+$(document).on('click', 'a.scroll-to-top', function (e) {
+    $('html, body').stop().animate({scrollTop:0}, 100, 'swing');
+    e.preventDefault();
+});
 
 
 
@@ -63,6 +45,18 @@
 // Dark Mode
 
 var darkSwitch = document.getElementById("darkSwitch");
+
+// darkSwitch.addEventListener('click', function () {
+//     console.log('yes')
+//     if (darkSwitch) {
+//         initTheme();
+//         darkSwitch.addEventListener("change", function () {
+//           resetTheme();
+//         });
+//         console.log('bonjour')
+//       }
+// })
+
 window.addEventListener("load", function () {
   if (darkSwitch) {
     initTheme();
@@ -91,5 +85,3 @@ function resetTheme() {
     localStorage.removeItem("darkSwitch");
   }
 }
-
-console.log('coucou')
